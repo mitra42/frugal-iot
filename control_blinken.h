aa25a16f966f808fac37d3d5510f3bc5d195e59a
mitra42 <mitra@mitra.biz>
2024-09-25 19:33:39 +0800

    Copy actuator_blinken to control_blinken and actuator_led

---
+++ control_blinken.h
@@ -0,0 +1,14 @@
+#ifndef ACTUATOR_BLINKEN_H
+#define ACTUATOR_BLINKEN_H
+
+/* Configuration options
+ * Required: ACTUATOR_BLINKEN_MS
+ * Optional: ACTUATOR_BLINKEN_DEBUG ACTUATOR_BLINKEN_PIN - defaults
+*/
+
+
+namespace aBlinken {
+void setup();
+void loop();
+} // namespace aBlinken
+#endif // ACTUATOR_BLINKEN_H
