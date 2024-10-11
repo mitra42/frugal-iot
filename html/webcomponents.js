bb9ff0b5d54b63e5b2208d77e06f19e54724c4bd
mitra42 <mitra@mitra.biz>
2024-10-11 14:47:55 +0800

    21 - add mqtt-toggle element that can be used for switching - not yet tested.

--- html/webcomponents.js
+++ html/webcomponents.js
@@ -2,12 +2,14 @@
  * Simple MQTT template for FrugalIoT
  */
 import {EL, HTMLElementExtended, getUrl} from './node_modules/html-element-extended/htmlelementextended.js';
-import mqtt from './node_modules/mqtt/dist/mqtt.esm.js';
+import mqtt from './node_modules/mqtt/dist/mqtt.esm.js'; // https://www.npmjs.com/package/mqtt
 
 var mqtt_client;
 var mqtt_subscriptions = [];
+let unique_id = 1; // Just used as a label for auto-generated elements
 
 function mqtt_subscribe(topic, cb) {
+  console.log("Subscribing to ", topic);
   mqtt_subscriptions.push({topic, cb});
   mqtt_client.subscribe(topic, (err) => { if (err) console.error(err); })
 }
@@ -72,10 +74,13 @@ class MqttClient extends HTMLElementExtended {
         //mqtt_client.end();
       });
     } else {
-      console.log("XXX already started connection")
+      // console.log("XXX already started connection") // We expect this, probably one time
     }
   }
-
+  publish(topic, msg, options) {
+    console.log("Publishing", topic, " ", msg);
+    mqtt_client.publish(topic, msg, options);
+  }
   // TODO display some more about the client and its status.
   render() {
     console.log("render");
@@ -94,26 +99,28 @@ class MqttElement extends HTMLElementExtended {
   shouldLoadWhenConnected() { return !!mqtt_client; }
 }
 
-class MqttText extends MqttElement {
-  constructor() {
-    super();
-    console.log("Creating MqttText")
-  }
-  static get observedAttributes() {
-    return ['topic',]; }
-  message_received(topic, message) {
-    console.log("Setting ", topic, " to ", message );
-    this.state.value = message;
-    this.renderAndReplace();
-  }
-  shouldLoadWhenConnected() { return !!mqtt_client && !this.state.subscribed; }
+class MqttReceiver extends MqttElement {
+  // constructor() { super(); }
+  static get observedAttributes() { return ['topic',]; }
+  // TODO - think this could be super() &&  !this.state.subscribed;
+  //shouldLoadWhenConnected() { return !!mqtt_client && !this.state.subscribed; }
+  shouldLoadWhenConnected() { return super.shouldLoadWhenConnected() && !this.state.subscribed; }
   loadContent() {
-    console.log("MqttText.loadContent");
     this.state.subscribed = true;
     mqtt_subscribe(this.state.topic, this.message_received.bind(this));
   }
+  valueSet() { } // Intended to be subclassed
+
+  message_received(topic, message) {
+    console.log("Setting ", topic, " to ", message );
+    this.state.value = message;
+    this.valueSet();
+    this.renderAndReplace();
+  }
+}
+class MqttText extends MqttReceiver {
+  // constructor() { super(); }
   render() {
-    console.log("Rendering MqttText");
     return [
       EL('div', {},[
         EL('span',{class: 'demo', textContent: this.state.topic + ": "}),
@@ -123,3 +130,42 @@ class MqttText extends MqttElement {
   }
 }
 customElements.define('mqtt-text', MqttText);
+
+class MqttTransmitter extends MqttReceiver {
+  static get observedAttributes() { return MqttReceiver.observedAttributes.concat(['retain', 'qos']); }
+  static get integerAttributes() { return MqttReceiver.integerAttributes.concat(['retain', 'qos']) };
+}
+
+// TODO this will probably split to insert a MqttTransmitter as subclass of MqttReceiver
+class MqttToggle extends MqttTransmitter {
+  constructor() {
+    super();
+  }
+  valueSet() {
+    this.state.indeterminate = false;
+  }
+  static get observedAttributes() {
+    return MqttTransmitter.observedAttributes.concat(['value']);
+  }
+  onChange(e) {
+    //console.log("Changed"+e.target.checked);
+    this.state.value = e.target.checked;
+    mqtt_client.publish(this.state.topic, this.state.value, { retain: this.state.retain, qos: this.state.qos});
+  }
+
+  render() {
+    //this.state.changeable.addEventListener('change', this.onChange.bind(this));
+    return [
+      EL('div', {},[
+        EL('input', {type: 'checkbox', id: 'checkbox'+ (++unique_id) ,
+          checked: !!this.state.value, indeterminate: typeof(this.state.value) == "undefined",
+          onchange: this.onChange.bind(this)}),
+        // TODO check how can test whether value is set and set indeterminate if not set (but not if false)
+        EL('label', {for: 'checkbox'+unique_id }, [
+           EL('slot', {}),
+        ]),
+      ]),
+    ];
+  }
+}
+customElements.define('mqtt-toggle', MqttToggle);
