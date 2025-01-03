761cb0a163519a8cd6b46f2989ec5bbfd9fbca61
mitra42 <mitra@mitra.biz>
2025-01-03 19:59:20 +1100

    Sensor Sensor_Float and Sensor_Uint16 classes - compiles

---
+++ sensor.cpp
@@ -0,0 +1,51 @@
+/*
+  Base class for sensors
+*/
+
+#include "_settings.h"  // Settings for what to include etc
+#include <Arduino.h>
+#include "sensor.h"
+#include "system_mqtt.h"
+
+Sensor *Sensor::first = NULL;
+
+Sensor::Sensor() : Frugal_Base() {  // TODO-25 might set topic here
+  next = first; // Will be null the first time
+  first = this; // Static first points to new top of linked list
+};
+Sensor_Float::Sensor_Float() : Sensor() {  // TODO-25 might set topic here
+};
+Sensor_Uint16::Sensor_Uint16() : Sensor() {  // TODO-25 might set topic here
+};
+
+void Sensor_Float::set(float newvalue) {
+  if (changed(newvalue)) {
+    value = newvalue;
+    act();
+  }
+}
+void Sensor_Uint16::set(uint16_t newvalue) {
+  if (changed(newvalue)) {
+    value = newvalue;
+    act();
+  }
+}
+// TODO_C++_EXPERT is there a (pretty!) way to do these next two pairs of textually similar as one definition or a macro or something. 
+bool Sensor_Float::changed(float newvalue) {
+  return (newvalue == value);
+}
+bool Sensor_Uint16::changed(uint16_t newvalue) {
+  return (newvalue == value);
+}
+void Sensor_Float::act() {
+      if (topic) {
+      xMqtt::messageSend(topic, value, retain, qos); // Note messageSend will convert value to String and expand topic
+    }
+}
+void Sensor_Uint16::act() {
+      if (topic) {
+      xMqtt::messageSend(topic, value, retain, qos); // Note messageSend will convert value to String and expand topic
+    }
+}
+
+
---
+++ sensor.h
@@ -0,0 +1,33 @@
+#ifndef SENSOR_H
+#define SENSOR_H
+
+#include <Arduino.h>
+#include "_base.h"
+
+class Sensor : public Frugal_Base {
+  public:
+    char* topic = NULL; // Topic to send to
+    const bool retain = false;
+    const int qos = 0; // Default to no guarrantee of delivery
+
+    Sensor();
+    static Sensor *first;
+}; // Class Sensor
+
+class Sensor_Float : public Sensor {
+    float value; 
+    Sensor_Float(); 
+    virtual float read();
+    virtual void set(float newvalue);
+    virtual bool changed(float newvalue);
+    virtual void act();
+};
+class Sensor_Uint16 : public Sensor {
+    uint16_t value;
+    Sensor_Uint16();
+    virtual uint16_t read();
+    virtual void set(uint16_t newvalue);
+    virtual bool changed(uint16_t newvalue);
+    virtual void act();
+};
+#endif // SENSOR_H
