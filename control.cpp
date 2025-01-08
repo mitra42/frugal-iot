b936b68a1230ee1185c659f6c2e837a85aeb996e
mitra42 <mitra@mitra.biz>
2025-01-09 08:04:41 +1100

    25 - starting compilation

--- control.cpp
+++ control.cpp
@@ -16,11 +16,6 @@
   #define CONTROL_DEBUG
 #endif
 
-std::vector<Control*> controls;
-
-class Control : public Frugal_Base {
-  public:
-}
 class IO {
     float value;
     String const * topic; // Topic currently listening to for input1Value
@@ -28,7 +23,8 @@ class IO {
     IO(float v, String const * t = NULL, String const * const c = NULL);
     virtual void setup();
     void dispatch(String &topic, String &payload); // Just checks control
-}
+};
+/*
 IO::IO(float v, String const * t = NULL, String const * const c = NULL): value(v), topic(t), control(c) {
   topic = xMqtt::topic_expand(topic);
   control = xMqtt::topic_expand(control);
@@ -46,6 +42,8 @@ void IO::dispatch(String &t, String &p) {
     }
   }
 }
+*/
+/*
 class IN : IO {
   IN(float v, String const * t = NULL, String const * const c = NULL);  
   bool dispatched(String &topic, String &payload); // For IN checks 
@@ -67,14 +65,21 @@ bool IN::dispatched(String &t, String &p) {
   }
   return false; 
 }
-
-
+*/
+/*
 class OUT : IO {
   OUT(float v, String const * t = NULL, String const * const c = NULL);  
 };
 OUT::OUT(float v, String const * t = NULL, String const * const c = NULL) : IO(v,t,c) {}
 // OUT::setup() - note OUT does not subscribe to the topic, it only sends on the topic
 // OUT::dispatch() - uses IO since wont be a topic, 
+
+OUT::set(float newvalue) {
+  if (newvalue != value) {
+    value == newvalue
+    Mqtt->send(topic, value);
+  }
+}
 class Control_3x3x3 : public Control {
   public:
     typedef std::function<void(void)> TCallback;
@@ -85,12 +90,16 @@ class Control_3x3x3 : public Control {
     Control_3x3x3(IO inputs[3], IO outputs[3], Tcallback actions[3]);
     setup();
 }
-OUT::set(float newvalue) {
-  if (newvalue != value) {
-    value == newvalue
-    Mqtt->send(topic, value);
-  }
+extern std::vector<Control*> controls;
+
+
+*/
+/*
+class Control : public Frugal_Base {
+  public:
 }
+*/
+/*
 Control_3x3x3::Control_3x3x3(IO i[3], IO o[3], Tcallback a[3]): Control, inputs(i), outputs(o), actions(a) { 
   controls.push_back(this); 
 }
@@ -113,21 +122,40 @@ Control_3x3x3::dispatch() {
     }
   }
 }
-
+std::vector<Control*> controls;
+*/
+/*
 // Example definition
-IO in1(0, "humidity, "humidity_sensor_control");
-IO in2(50, "limit");
-IO in3(5, "hysterisis");
-IO inputs[3] = [in1,in2,in3];
-IO out1(0, "ledbuiltin", "relay_control"); // Default to control LED, controllable via "relay_control")
-IO* outputs[3] = [out1,NULL,NULL];
+IN ch_in1(0, "humidity, "humidity_sensor_control");
+IN ch_in2(50, "limit");
+IN ch_in3(5, "hysterisis");
+IN ch_ins[3] = [ch_in1,ch_in2,ch_in3];
+OUT ch_out1(0, "ledbuiltin", "relay_control"); // Default to control LED, controllable via "relay_control")
+OUT* ch_outs[3] = [ch_out1,NULL,NULL];
 Tcallback hysterisisAction(Control_3x3x3* self) { //TODO-25 need to figure out how to access variables, maybe pass this pointer
   const float hum = self->inputs[0].value;
   const float lim = self->inputs[1].value;
-  const float histerisis = self->inputs[2].value;
-  if (hum > (lim + histerisis)) { self->outputs[0].set(1); }
-  if (hum < (lim - histerisis)) { self->outputs[0].set(0); }
+  const float hysterisis = self->inputs[2].value;
+  if (hum > (lim + hysterisis)) { self->outputs[0].set(1); }
+  if (hum < (lim - hysterisis)) { self->outputs[0].set(0); }
   // If  lim-histerisis < hum < lim+histerisis then don't change setting
 }
-actions[3] = [hysterisisAction, NULL, NULL]
-Control_3x3x3* humcontroller = new Control_3x3x3(inputs, outputs, actions);
+Tcallback ch_acts[3] = [hysterisisAction, NULL, NULL]
+Control_3x3x3* control_humidity = new Control_3x3x3(ch_ins, ch_outs, ch_acts);
+*/
+/*
+// Example for blinken 
+long unsigned lastblink; // Note local variable in same contex as control_blinken
+IN cb_in1 = [1,"blinkspeed",NULL];
+OUT cb_out1 = [0, "ledbuiltin", NULL];
+IN* cb_ins[3] = [cb_in1, NULL, NULL];
+OUT* cb_outs[3] = [cb_out1, NULL, NULL];
+Tcallback blink(Control_3x3x3* self) {
+  if (lastblink + (self->inputs[0].value*1000)) < millis() { 
+    self->outputs[0].set(!!self->outputs[0].value);
+    lastblink = millis();
+  }
+}
+Tcallback cb_acts[3] = [xxx, NULL, NULL]
+Control_3x3x3* control_blinken = new Control_3xx3x3(cb_ins, cb_outs, cb_acts)
+*/
