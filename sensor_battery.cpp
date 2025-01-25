43a9c507ef3b497e8ee65b9ad776adf7bb3d1226
mitra42 <mitra@mitra.biz>
2025-01-25 11:47:19 +1100

    sensor_battery for ESP8266 - untested

--- sensor_battery.cpp
+++ sensor_battery.cpp
@@ -16,9 +16,28 @@
 
 Sensor_Battery::Sensor_Battery(const uint8_t pin_init) : Sensor_Analog(pin_init, 0, SENSOR_BATTERY_TOPIC, SENSOR_BATTERY_MS) { }
 
+#define VOLTAGE_DIVIDER 2 // Maybe board specific but everything I've seen is two equal resistors
+
+#ifdef ESP8266 // analogReadMilliVolts not available
+  #define ANALOG_READ_RANGE 256
+  #define VCC_MILLIVOLTS 3300
+
+  // Note the ESP32 function returns uint32_t which makes no sense given max battery is 5000
+  uint16_t analogReadMilliVolts(uint8_t pin) {
+    /* THere may be a reason to do it this way to get float calcs ?
+    const float VCC_Volt = 5.000; // ( 5v for 8bits Arduino boards, 3.3v for ESP, STM32 and SAMD )
+    const analogReadRange = 1024;  // for Arduino boards
+    uint16_t analogValue = analogRead(pin);
+    uint16_t milliVolts = (VCC_Volt * 1000 * analogValue) /  analogReadRange;
+    */
+    static const float multiplier = VCC_MILLIVOLTS * VOLTAGE_DIVIDER / ANALOG_READ_RANGE ; 
+    uint16_t analogValue = analogRead(pin);
+    return analogValue * multiplier; 
+  }
+#endif //ESP8266
+
 uint16_t Sensor_Battery::read() {
-  // Shifted left two because reading is from divide by 2 resistors
-  return analogReadMilliVolts(pin) << 1; // Note this returns uiunt32_t which makes no sense given max value is 5*1000 = 5000
+  return analogReadMilliVolts(pin) * VOLTAGE_DIVIDER; // Note this returns uiunt32_t which makes no sense given max value is 5*1000 = 5000
 }
 
 #endif // SENSOR_BATTERY_WANT
