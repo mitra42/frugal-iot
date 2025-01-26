87176e8fb592c59e266e393a84eacfd20d76b869
mitra42 <mitra@mitra.biz>
2025-01-26 16:47:02 +1100

    37 OTA Change certificate - connects but not updating yet.

--- system_ota.cpp
+++ system_ota.cpp
@@ -53,37 +53,38 @@
 #ifdef ESP32 //See https://github.com/espressif/arduino-esp32/blob/master/libraries/Update/examples/HTTPS_OTA_Update/HTTPS_OTA_Update.ino
   #include "HttpsOTAUpdate.h"
 
-//TODO-37 this may need to be root as changes every 3 months
-  static const char *server_certificate = "-----BEGIN CERTIFICATE-----\n"
-    "MIIFHDCCBASgAwIBAgISA8cagukxHzEHVEt1xMuMm0J1MA0GCSqGSIb3DQEBCwUA\n"
-    "MDMxCzAJBgNVBAYTAlVTMRYwFAYDVQQKEw1MZXQncyBFbmNyeXB0MQwwCgYDVQQD\n"
-    "EwNSMTAwHhcNMjQxMTIxMTUxMjU1WhcNMjUwMjE5MTUxMjU0WjAUMRIwEAYDVQQD\n"
-    "EwltaXRyYS5iaXowggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCgk0Ch\n"
-    "4DPbDclT707OLX3nfUoE1F59LfPsjJ6x3MhVyGOleGSdSfGtorcAJh2HnMRD2gem\n"
-    "m1kd1XQX2/dyknugTUkah9RffPtqbFv2b532quFIxzM6AM4a2akHxxnkfWLhdQ2g\n"
-    "lYqCi42ojFKjZ99v+uu9Vu6Xj64FH55xNs7Q5xVX0qvQQpdcfbdfoDP4RJ6pcX8c\n"
-    "qBTqNDo4wt7CY02pXcFLxVbZ4i9+N/iqvt7fM6gop8FTZGWR2t3GcfS7L7uDoH5C\n"
-    "iU/lZd87S87jjVsem95O/nNmhE0U7WqnvA6JKjtMngDrPet9E/SgALzbIlT9EJ0+\n"
-    "a9GZn4Lz/y5R3VcDAgMBAAGjggJHMIICQzAOBgNVHQ8BAf8EBAMCBaAwHQYDVR0l\n"
-    "BBYwFAYIKwYBBQUHAwEGCCsGAQUFBwMCMAwGA1UdEwEB/wQCMAAwHQYDVR0OBBYE\n"
-    "FHNE0rBUWzs9PvJ/Ml3+6zSB1zn+MB8GA1UdIwQYMBaAFLu8w0el5LypxsOkcgwQ\n"
-    "jaI14cjoMFcGCCsGAQUFBwEBBEswSTAiBggrBgEFBQcwAYYWaHR0cDovL3IxMC5v\n"
-    "LmxlbmNyLm9yZzAjBggrBgEFBQcwAoYXaHR0cDovL3IxMC5pLmxlbmNyLm9yZy8w\n"
-    "TgYDVR0RBEcwRYIJKi5lcGEuZWR1ggsqLm1pdHJhLmJpeoIXKi5uYXR1cmFsaW5u\n"
-    "b3ZhdGlvbi5vcmeCB2VwYS5lZHWCCW1pdHJhLmJpejATBgNVHSAEDDAKMAgGBmeB\n"
-    "DAECATCCAQQGCisGAQQB1nkCBAIEgfUEgfIA8AB2AM8RVu7VLnyv84db2Wkum+ka\n"
-    "cWdKsBfsrAHSW3fOzDsIAAABk09+HV4AAAQDAEcwRQIgayArqpxhykq4F/6HhAd1\n"
-    "lJQiYNegtM1xdiTi13OmlasCIQDQgyRAwmMKeMIXRcZqNqTu8WsgQPWyTQ/qOxIh\n"
-    "bebKXAB2AOCSs/wMHcjnaDYf3mG5lk0KUngZinLWcsSwTaVtb1QEAAABk09+HTgA\n"
-    "AAQDAEcwRQIhAN879ukkf6FKvuCtkfa3eNeqsg8e3K08P+smoPr7NiC6AiAX4rSM\n"
-    "9EVSW2A2DIG3D50vfz1qdiYBcrhKUoBl4PRRyjANBgkqhkiG9w0BAQsFAAOCAQEA\n"
-    "LCCl0UMkccYHd8pui39aXU3+ud28kVEfhD/nDHaOUrH8rw8M+CO1SILTRXB0qp3L\n"
-    "G42fusAXpPmwQPwcprULNr9nmmnL+s7UAXfhMCp3jcfTDunMsKVdP7/YABgGPKRb\n"
-    "vsKzCBlTWA/WSBvaAGBXA+p0/EQSL6tIkU0CvfERb9XeTYqKj56KLinrrzxjfciG\n"
-    "p/7ucCS/LtD475P2A+BVyrAbabhVAc9lXue6ZWOa70XaXj5J73skxB2/hezGbk7G\n"
-    "PqE1zq4PVi/TwNaKRbcLDFcAZZddIxxqOe0FO+P02U/2g4St0xcchmfsNSg2izM4\n"
-    "qs8ZO9aIZ7ceCyCRORd8zA==\n"
-    "-----END CERTIFICATE-----";
+const char* rootCACertificateForNaturalInnovation = \
+    "-----BEGIN CERTIFICATE-----\n"
+"MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n"
+"TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n"
+"cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n"
+"WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n"
+"ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY\n"
+"MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc\n"
+"h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+\n"
+"0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U\n"
+"A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW\n"
+"T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH\n"
+"B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC\n"
+"B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv\n"
+"KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn\n"
+"OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn\n"
+"jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw\n"
+"qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI\n"
+"rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV\n"
+"HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq\n"
+"hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL\n"
+"ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ\n"
+"3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK\n"
+"NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5\n"
+"ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur\n"
+"TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC\n"
+"jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc\n"
+"oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq\n"
+"4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA\n"
+"mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n"
+"emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n"
+"-----END CERTIFICATE-----\n";
 
   static HttpsOTAStatus_t otastatus;
 
@@ -189,7 +190,7 @@ void updateAndReboot(bool sketch) {
     #endif
 
     #ifdef ESP32
-      HttpsOTA.begin(url, server_certificate);
+      HttpsOTA.begin(url, rootCACertificateForNaturalInnovation);
       // TODO-37 probably put this in its own loop()
       while (true) {
         otastatus = HttpsOTA.status();
@@ -224,6 +225,7 @@ void updateAndReboot(bool sketch) {
 
 void setup() {
   #ifdef ESP32 // TODO-37 move this to system_time
+  /* DOesnt compile - replace with John's code but put in system_time
     esp_sntp_config_t config = ESP_NETIF_SNTP_DEFAULT_CONFIG("pool.ntp.org");
     esp_netif_sntp_init(&config);
     if (esp_netif_sntp_sync_wait(pdMS_TO_TICKS(10000)) != ESP_OK) {
@@ -240,6 +242,7 @@ void setup() {
     localtime_r(&now, &timeinfo);
     strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
     Serial.printf("The current date/time in Shanghai is: %s", strftime_buf);
+  */
   #endif
   updateAndReboot(true); // Note this must be after WiFi has connected  and ideally before MQTT or Discovery
 }
