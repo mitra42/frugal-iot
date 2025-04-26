f6cc8953388a2329a1aed7a4ca991458eb7c70b0
mitra42 <mitra@mitra.biz>
2025-04-26 19:31:51 +1000

    110ens160aht21 started

---
+++ src/sensor_ens160aht21.cpp
@@ -0,0 +1,14 @@
+/* Frugal IoT - Sensor - ENS160 AHT21
+* 
+* This is a sensor that uses the ENS160 and AHT21 chips to read the air quality.
+*
+* Info: https://www.instructables.com/ENS160-AHT21-Sensor-for-Arduino/
+* Source: https://www.aliexpress.us/w/wholesale-ens160%2Baht21.html?
+* Issue: https://github.com/mitra42/frugal-iot/issues/101
+* Reddit: https://www.reddit.com/r/arduino/comments/12ulwo2/has_anyone_been_able_to_get_ens160aht21_working/
+* 
+* Important notes extracte from above.
+* Vin is 5V - dont use 3.3V its an output from the regulator
+
+*/
+
