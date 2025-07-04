7a3bf374706f457722f9d3047a015b20d7315f5a
mitra42 <mitra@mitra.biz>
2025-07-04 18:19:17 +1000

    141 - working on Arduino - tested on SHT only

--- examples/sonoff/src/main.cpp
+++ examples/sonoff/src/main.cpp
@@ -13,12 +13,22 @@ void setup() {
   frugal_iot.startSerial(); // Encapsulate setting up and starting serial
 
   // Override MQTT host, username and password if you have an "organization" other than "dev" (developers)
-  frugal_iot.system->add(new System_MQTT("frugaliot.naturalinnovation.org", "dev", "public"));
+  frugal_iot.configure_mqtt("frugaliot.naturalinnovation.org", "dev", "public");
+
+  // Configure power handling - type, cycle_ms, wake_ms 
+  // power will be awake wake_ms then for the rest of cycle_ms be in a mode defined by type 
+  // Loop= awake all the time; 
+  // Light = Light Sleep; 
+  // LightWiFi=Light + WiFi on (not working); 
+  // Modem=Modem sleep - works but negligable power saving
+  // Deep - works but slow recovery and slow response to UX so do not use except for multi minute cycles. 
+  frugal_iot.configure_power(Power_Loop, 30000, 30000); // Take a reading every 30 seconds - awake all the time
+
   // system_oled and actuator_ledbuiltin added automatically on boards that have them.
 
   // Add local wifis here, or see instructions in the wiki for adding via the /data
   frugal_iot.wifi->addWiFi(F("mywifissid"),F("mywifipassword"));
-
+  
   // Relay on Sonoff is on pin 12
   frugal_iot.actuators->add(new Actuator_Digital("relay", "Relay", 12, "purple"));
   ControlHysterisis* cb = new ControlHysterisis("control", "Control", 50, 1, 0, 100);
