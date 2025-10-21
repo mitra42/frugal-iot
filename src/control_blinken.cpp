956bbae1f19a689675a8355bc9dc0120b0948dd9
mitra42 <mitra@mitra.biz>
2025-10-21 20:16:35 +1100

    Power: fixed sleepSafeMillis which wasnt tracking in deep sleep, created sleep safe timers and reworked blinken to use them

--- src/control_blinken.cpp
+++ src/control_blinken.cpp
@@ -19,24 +19,28 @@ ControlBlinken::ControlBlinken (const char* const id, const char * const name, f
   std::vector<OUT*> {
     new OUTbool(id, "out", name, false, "black", true), 
   }
-), blinkOn(secsOn * 1000), blinkOff(secsOff * 1000) {
+), blinkOn(secsOn * 1000), blinkOff(secsOff * 1000), timer_index(frugal_iot.powercontroller->timer_next()) {
   #ifdef CONTROL_BLINKEN_DEBUG
     debug("ControlBlinken after instantiation");
     Serial.print(F("ControlBlinken: blinkOn: ")); Serial.print(blinkOn); Serial.print(F(" blinkOff: ")); Serial.println(blinkOff);
   #endif
 };
 
+void ControlBlinken::timer_set(unsigned long t) {
+  frugal_iot.powercontroller->timer_set(timer_index, t);
+}
+
 void ControlBlinken::act() {
   // If calling act, then we know blinkSpeed changed
   blinkOn = inputs[0]->floatValue() * 1000;
   blinkOff = inputs[1]->floatValue() * 1000;
-  nextBlinkTime = frugal_iot.powercontroller->sleepSafeMillis() + (outputs[0]->boolValue() ? blinkOn : blinkOff) ; // Blink after new blink time
+  timer_set(outputs[0]->boolValue() ? blinkOn : blinkOff);
 }
 
 void ControlBlinken::loop() {
-  if (nextBlinkTime <= frugal_iot.powercontroller->sleepSafeMillis()) {
+  if (frugal_iot.powercontroller->timer_expired(timer_index)) {
     bool next = !outputs[0]->boolValue();
     ((OUTbool*)outputs[0])->set(next); // Will send message
-    nextBlinkTime = frugal_iot.powercontroller->sleepSafeMillis() + (next ? blinkOn : blinkOff);
+    timer_set(next ? blinkOn : blinkOff);
   }
 }
