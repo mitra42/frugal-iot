722931804a6691214397753fd978c80bdf7ae756
mitra42 <mitra@mitra.biz>
2025-12-07 15:58:43 +1100

    OTA cleanup including moving ota.md to wiki

--- examples/loramesher/loramesher.ino
+++ examples/loramesher/loramesher.ino
@@ -32,6 +32,8 @@ void setup() {
   // Deep - works but slow recovery and slow response to UX so do not use except for multi minute cycles. 
   frugal_iot.configure_power(Power_Loop, 30000, 30000); // Take a reading every 30 seconds - awake all the time
 
+  // actuator_oled and actuator_ledbuiltin added automatically on boards that have them.
+
   // Add local wifis here, or see instructions in the wiki for adding via the /data
   frugal_iot.wifi->addWiFi(F("mywifissid"),F("mywifipassword"));
   
