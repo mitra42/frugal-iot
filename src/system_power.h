967da289d156a99e40d2db607d9f4fdb9d41bd12
mitra42 <mitra@mitra.biz>
2025-12-25 16:30:07 +1100

    23 Power Rework - get rid of multiple classes, use mode constant only - may move again to granular constants (like pause-WiFi)

--- src/system_power.h
+++ src/system_power.h
@@ -9,7 +9,7 @@
 
 // TO-ADD-POWER
 enum System_Power_Type { 
-  Power_Loop,         // Standard loop, no waiting
+  Power_Loop = 0,     // Standard loop, no waiting
   Power_Light,        // Does a Light sleep
   Power_LightWiFi,    // Like Light, but wakes on WiFi, which menas it SHOULD keep WiFi alive. (poor power savings currently - possibly because of Uart=Serial)
   Power_Modem,        // ESP32 Modem sleep mode - need to check what this means
@@ -18,6 +18,7 @@ enum System_Power_Type {
 
 class System_Power_Mode : public System_Base {
   public:
+    System_Power_Type mode; 
     uint8_t timer_next(); // Return an index to a timer that can be used
     void timer_set(uint8_t i, unsigned long t);
     bool timer_expired(uint8_t i); 
@@ -28,14 +29,14 @@ class System_Power_Mode : public System_Base {
     #else // Only needed/valid on ESP32 where have saved millis_offset in RTCs memory
       unsigned long sleepSafeMillis() { return millis(); }
     #endif
-    static System_Power_Mode* create(System_Power_Type, unsigned long cycle_ms, unsigned long wake_ms);
+    System_Power_Mode(const System_Power_Type mode, unsigned long cycle_ms, unsigned long wake_ms);
   protected: // Move any of these needed to public above
     unsigned long timer(uint8_t i); // Return value of timer
     unsigned long nextSleepTime = 0; // Next time to sleep in millis() (NOT offseted) - set in constructor, updated in maybeSleep()
     unsigned long cycle_ms; // Time for each cycle (wake + sleep)
     unsigned long wake_ms; // Time to stay awake during each cycle
-    System_Power_Mode(const char* name, unsigned long cycle_ms, unsigned long wake_ms);
     void setup() override;
+    void LightWifi_setup();
     unsigned long sleep_ms() { return cycle_ms - wake_ms; }
     unsigned long sleep_us() { return sleep_ms() * 1000ULL; }
     //virtual void configure(); // Typically called from setup() but might also be called if switch modes
@@ -47,60 +48,7 @@ class System_Power_Mode : public System_Base {
   private:
     uint8_t timer_index;
 };
-class System_Power_Loop : public System_Power_Mode {
-  public:
-    System_Power_Loop(unsigned long cycle_ms, unsigned long wake_ms);
-    //void configure(); // Typically called from setup() but might also be called if switch modes
-    //void setup() override;
-    void prepare();  // Does nothing in Loop
-    void sleep(); // Does nothing in Loop
-    void recover(); // Does nothing in Loop
-};
-#ifdef ESP32 // Deep, Light and Modem sleep specific to ESP32
-class System_Power_Light : public System_Power_Mode {
-  public:
-    System_Power_Light(unsigned long cycle_ms, unsigned long wake_ms);
-    //void configure(); // Typically called from setup() but might also be called if switch modes
-    void prepare() override;
-    void sleep() override;
-    void recover() override;
-};
-#endif
-#ifdef ESP32 // Deep, Light and Modem sleep specific to ESP32
-
-class System_Power_Deep : public System_Power_Mode {
-  public:
-    System_Power_Deep(unsigned long cycle_ms, unsigned long wake_ms);
-   // void configure(); // Typically called from setup() but might also be called if switch modes
-    void setup() override;
-    //void prepare() override; // Use superclass
-    void sleep() override;
-    void recover() override;
-};
-#endif
-#ifdef ESP32 // Deep, Light and Modem sleep specific to ESP32
-
-class System_Power_LightWiFi : public System_Power_Mode {
-  public:
-    System_Power_LightWiFi(unsigned long cycle_ms, unsigned long wake_ms);
-    //void configure(); // Typically called from setup() but might also be called if switch modes
-    void setup() override;
-    //void prepare() override;
-    void sleep() override;
-    void recover() override;
-};
-#endif
-#ifdef ESP32 // Deep, Light and Modem sleep specific to ESP32
-
-class System_Power_Modem : public System_Power_Mode {
-  public:
-    System_Power_Modem(unsigned long cycle_ms, unsigned long wake_ms);
-    //void configure(); // Typically called from setup() but might also be called if switch modes
-    //void prepare() override;
-    void sleep() override;
-    //void recover() override;
-};
-#endif
-extern System_Power_Mode* powerController;
 
 #endif // SYSTEM_POWER_H
+
+// REVIEW NEW POWER ORG  DONE BELOW  DO ABOVE.  ^^^^^^^^
