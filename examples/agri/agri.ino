bd15ca4f092f7edd2df5fd37789723e8ebe3e3e9
mitra42 <mitra@mitra.biz>
2026-02-24 13:01:59 +0800

    Agri - Deep sleep between readings - should be parameterized

--- examples/agri/agri.ino
+++ examples/agri/agri.ino
@@ -26,8 +26,8 @@ void setup() {
   // Power_LightWiFi=Light + WiFi on (not working); 
   // Power_Modem=Modem sleep - works but negligable power saving
   // Power_Deep - works but slow recovery and slow response to UX so do not use except for multi minute cycles. 
-  //frugal_iot.configure_power(Power_Deep, 600000, 30000); // Take a reading every 10 mins deep sleep between
-  frugal_iot.configure_power(Power_Loop, 10000, 10000); // For debugging sensors - 10 second loop
+  frugal_iot.configure_power(Power_Deep, 600000, 30000); // Take a reading every 10 mins deep sleep between
+  //frugal_iot.configure_power(Power_Loop, 10000, 10000); // For debugging sensors - 10 second loop
   
   // Encapsulate setting up and starting serial and read main config also checks power ok.
   // This has to happen AFTER battery and power are setup, and before mqtt and adding sensors actuators etc. 
